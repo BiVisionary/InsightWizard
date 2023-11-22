65f54d1202a97b49fbb883b4e0c6b1dd73268970
BiVisionary <150253785+BiVisionary@users.noreply.github.com>
2023-11-22 17:57:36 +0530

    Update InsightWizard_Streamlit_app.py

--- InsightWizard_Streamlit_app.py
+++ InsightWizard_Streamlit_app.py
@@ -2,70 +2,13 @@ import streamlit as st
 import pandas as pd
 import os
 import tempfile
-import requests
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import json
-import base64
-import re
-
-def generate_chart_html(langchain_response):
-    chart_data = [['Role', 'Edits']]
-    for key, value in langchain_response.items():
-        role = key[1]
-        edits = value
-        chart_data.append([role, edits])
-
-    chart_code = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <script type="text/javascript">
-        google.charts.load("current", {{packages: ["corechart"]}});
-        google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {{
-            var data = google.visualization.arrayToDataTable({json.dumps(chart_data)});
-
-            var options = {{
-                title: 'Edits Distribution Chart',
-                hAxis: {{title: 'Role'}},
-                vAxis: {{title: 'Edits'}}
-            }};
-
-            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-
-            chart.draw(data, options);
-        }}
-        </script>
-    </head>
-    <body>
-        <div id="chart_div" style="width: 800px; height: 400px;"></div>
-    </body>
-    </html>
-    """
-    return chart_code
-
-def extract_json_data(response):
-    match = re.search(r'{.*}', response)
-    if match:
-        json_data = match.group(0)
-        return json_data
-    return None
-
-def convert_response_to_json(response):
-    converted_data = {
-        'top_journals': [
-            {'journal': 'ANZF', 'edits': 1994},
-            {'journal': 'CAPR', 'edits': 1944},
-            {'journal': 'CAJE', 'edits': 1076},
-            {'journal': 'MAPS', 'edits': 977},
-            {'journal': 'CJCE', 'edits': 975}
-        ]
-    }
-    return json.dumps(converted_data)
+import io
+import altair as alt
+import openai
 
 def run_agents(openai_agent, csv_data, user_question):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_csv_file:
@@ -78,9 +21,47 @@ def run_agents(openai_agent, csv_data, user_question):
 
     return response
 
+def generate_dynamic_chart(data, chart_title="Dynamic Chart"):
+    dfs = []
+
+    def process_data(data, prefix=""):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                new_prefix = f"{prefix}_{key}" if prefix else key
+                process_data(value, new_prefix)
+        elif isinstance(data, list):
+            for i, item in enumerate(data):
+                new_prefix = f"{prefix}_{i}" if prefix else str(i)
+                process_data(item, new_prefix)
+        else:
+            if isinstance(data, str) or isinstance(data, int) or isinstance(data, float):
+                df = pd.DataFrame({'Value': [data]})
+                df['Role'] = prefix
+                dfs.append(df)
+
+    process_data(data)
+
+    if not dfs:
+        st.warning("No valid data found for chart.")
+        return None
+
+    combined_df = pd.concat(dfs)
+
+    chart = alt.Chart(combined_df).mark_bar().encode(
+        x=alt.X('Role:N', title='Category'),
+        y=alt.Y('Value:Q', title='Value'),
+        color=alt.Color('Role:N', title='Role')
+    ).properties(
+        title=chart_title,
+        width=400
+    )
+
+    return chart
+
 def main():
     load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY")
+    #st.write(os.getenv("OPENAI_API_KEY"))
 
     if api_key is None or api_key == "":
         st.error("OPENAI_API_KEY is not set")
@@ -100,39 +81,40 @@ def main():
         if user_question_llm is not None and user_question_llm != "":
             with st.spinner(text="In progress..."):
                 response_from_langchain = run_agents(openai_agent, csv_data, user_question_llm)
+                st.write(response_from_langchain)
 
                 if response_from_langchain:
+                    openai.api_key = api_key
 
+                    conversation = [
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": response_from_langchain},
+                        {"role": "user", "content": user_question}
+                    ]
 
-                    import openai
+                    response = openai.ChatCompletion.create(
+                        model="gpt-4",
+                        messages=conversation,
+                        max_tokens=250
+                    )
 
-                    # Set your OpenAI API key
-                    openai.api_key = "OPENAI_API_KEY"  # Replace with your API key
+                    chatgpt_response_text = response.choices[0].message.content
+                    st.write("Response from ChatGPT:")
+                    st.write(chatgpt_response_text)
 
-                    # ...
-
-                    if response_from_langchain:
-                        # Define a conversation with a system message (optional) and a user message
-                        conversation = [
-                            {"role": "system", "content": "You are a helpful assistant."},
-                            {"role": "user", "content": response_from_langchain},
-                            {"role": "user", "content": user_question}
-                        ]
-
-                        # Generate a response from ChatGPT
-                        response = openai.ChatCompletion.create(
-                            model="gpt-3.5-turbo",
-                            messages=conversation,
-                            max_tokens=150  # Adjust as needed
-                        )
-
-                        chatgpt_response_text = response.choices[0].message.content
-                        st.write("Response from ChatGPT:")
-                        st.write(chatgpt_response_text)
-
-
-                else:
-                    st.error("No response from Langchain")
+                    user_chart_data = st.text_area("Enter chart data in JSON format:")
+                    if st.button("Generate Dynamic Chart"):
+                        try:
+                            data = json.loads(user_chart_data)
+                            if data:
+                                chart = generate_dynamic_chart(data)
+                                st.altair_chart(chart, use_container_width=True)
+                        except json.JSONDecodeError:
+                            st.error("Invalid JSON data. Please provide data in a valid JSON format.")
+                        except openai.error.OpenAIError as e:  # Update this line
+                            st.error(f"Error from OpenAI: {str(e)}")
 
 if __name__ == "__main__":
     main()
+
+
